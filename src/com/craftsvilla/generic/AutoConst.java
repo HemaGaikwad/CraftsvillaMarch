1fb21c33f2ff6c1554c126608c79cd97a084b048
Zerobean <Zerobean@DESKTOP-01THEB1>
2019-04-06 21:10:28 +0530

    First commit

---
+++ src/com/craftsvilla/generic/AutoConst.java
@@ -0,0 +1,15 @@
+package com.craftsvilla.generic;
+
+public interface AutoConst 
+{
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE = ".//Drivers//chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE = ".//Drivers//geckodriver.exe";
+	
+	String FILEPATH = ".//InputData//Credentials.xlsx";
+	
+	String APP_URL = "https://www.craftsvilla.com/";
+
+}
---
+++ src/com/craftsvilla/testscripts/EmbroidedSareesScripts.java
@@ -0,0 +1,54 @@
+package com.craftsvilla.testscripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.craftsvilla.generic.BaseTest;
+import com.craftsvilla.pompages.CartPage;
+import com.craftsvilla.pompages.EmbroidedSareesPage;
+import com.craftsvilla.pompages.HomePage;
+import com.craftsvilla.pompages.ProductDescriptionPage;
+import com.craftsvilla.pompages.SareesPage;
+import com.craftsvilla.pompages.SignInPage;
+
+public class EmbroidedSareesScripts extends BaseTest
+{
+	
+	HomePage hp = null;
+	SareesPage sp = null;
+	EmbroidedSareesPage ep = null;
+	ProductDescriptionPage pd = null;
+	CartPage cp = null;
+	SignInPage sip= null;
+	
+	@Test
+	public void TC_01_BuyEmbroidedSaree()
+	{
+		hp = new HomePage(driver);
+		sp = new SareesPage(driver);
+		ep = new EmbroidedSareesPage(driver);
+		pd = new ProductDescriptionPage(driver);
+		cp = new CartPage(driver);
+		
+		
+		try
+		{
+			hp.goToSareesLink();
+			sp.clickOnEmbroidedSareesLink();
+			ep.goToProduct1538760092();
+			pd.checkProductName("Craftsvilla Blue Color Silk Designer Resham Embroidery Saree");
+			pd.clickOnBuyNow();
+			Assert.fail();
+			cp.setFirstName("Hema");
+			cp.setEmail("emailid");
+			cp.setMobile("88676574654");
+			
+		}
+		catch (Exception e)
+		{
+			
+		}
+		
+	}
+
+}
